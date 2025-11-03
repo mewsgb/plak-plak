17be9e71ce1557c0a019e2eb067b4d134cb6c107
mewsgb <kristina-voronina-1919@bk.ru>
2025-11-03 16:22:35 +0900

    Add files via upload

---
+++ praktika2/MyLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace MyLibrary
+{
+    public class Calculator
+    {
+        public void PrintHello()// без возвращаемого
+        {
+            Console.WriteLine("Hello world!");
+        }
+        public int Add(int a, int b)//  с возвращаемым
+        {
+            return a + b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public string о(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+    }
+
+}
---
+++ praktika2/praktika2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyLibrary;  // подключаем нашу DLL
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.PrintHello();// тестируем , правда я не понимаю че здесь вообще тестировать ну и ладно
+            Console.WriteLine($"5 + 3 = {calc.Add(5, 3)}");
+            Console.WriteLine($"4.5 * 2 = {calc.Multiply(4.5, 2)}");
+            Console.WriteLine(calc.о("User"));   
+        }
+    }
+}
